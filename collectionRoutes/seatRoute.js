3bdccf49ac1b555cd424a39548695f0ae3f295d2
ijTuhin <isratishu0209@gmail.com>
2023-05-17 17:50:26 +0600

    seat collection CRUD done

--- collectionRoutes/seatRoute.js
+++ collectionRoutes/seatRoute.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const seatSchema = require("../collectionSchemas/seatSchema.js");
 const Seat = new mongoose.model ("Seat", seatSchema);
-// GET
+
+// GET all
 router.get('/', async(req, res) => {
     await Seat.find({})
     .then((data)=>{
@@ -18,9 +19,10 @@ router.get('/', async(req, res) => {
     })
 })
 
-// GET by Id
-router.get('/:id', async(req, res) => {
-    /* await Seat.find({_id: req.params.id})
+
+// GET by room number
+router.get('/:room', async(req, res) => {
+    await Seat.find({room: req.params.room})
     .then((data)=>{
         res.status(200).json({
             data: data
@@ -30,11 +32,33 @@ router.get('/:id', async(req, res) => {
         res.status(400).json({
             error: "Oops! Something went wrong!"
         })
-    }) */
+    })
 })
 
 
-// POST
+// GET room number by member id
+router.get('/:member', async(req, res) => {
+    await Seat.find({member: { $in: [ req.params.member ] } })
+    .select({
+        member: 0,
+        vacant: 0,
+        vacancy: 0,
+        __v: 0
+    }) // To get all data, remove the select
+    .then((data)=>{
+        res.status(200).json({
+            data: data
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+})
+
+
+// Add new room
 router.post('/', async(req, res) => {
     const newSeat = new Seat(req.body);
     await newSeat
@@ -50,6 +74,7 @@ router.post('/', async(req, res) => {
         })
     })
 })
+
 
 // POST many
 router.post('/all', async(req, res) => {
@@ -67,7 +92,7 @@ router.post('/all', async(req, res) => {
 })
 
 
-// Remove user to seat
+// Remove user from seat
 router.put('/:room/remove-user', async(req, res) => {
     await Seat.updateOne({room: req.params.room}, {
         $pull: {
@@ -106,6 +131,8 @@ router.put('/:room/add-vacancy', async(req, res) => {
         })
     })
 })
+
+
 // Add user to seat
 router.put('/:room/insert-user', async(req, res) => {
     await Seat.updateOne({room: req.params.room}, {
@@ -147,9 +174,9 @@ router.put('/:room/remove-vacancy', async(req, res) => {
 })
 
 
-// DELETE
-router.delete('/:id', async(req, res) => {
-    /* await Seat.deleteOne({_id: req.params.id})
+// DELETE room
+router.delete('/:room', async(req, res) => {
+    await Seat.deleteOne({room: req.params.room})
     .then((data)=>{
         res.status(200).json({
             result: "Data deletion successful"
@@ -159,7 +186,7 @@ router.delete('/:id', async(req, res) => {
         res.status(400).json({
             error: "Oops! Something went wrong!"
         })
-    }) */
+    })
 })
 
 module.exports = router;
