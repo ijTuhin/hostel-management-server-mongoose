dd49f7682257a0706c20eb650d5a1a5b82f8dd8b
ijTuhin <isratishu0209@gmail.com>
2023-05-18 18:24:35 +0600

    POST and GET all, GET by Id done

--- collectionRoutes/mealRoute.js
+++ collectionRoutes/mealRoute.js
@@ -4,5 +4,97 @@ const router = express.Router();
 const mealSchema = require("../collectionSchemas/mealSchema")
 const Meal = new mongoose.model("Meal", mealSchema)
 
+// POST new meal order
+router.post('/order', async(req, res) => {
+    const newMeal = new Meal(req.body);
+    await newMeal
+    .save()
+    .then(()=>{
+        res.status(200).json({
+            success: "Insertion successful"
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+})
+
+
+// GET by id
+router.get('/:id', async(req, res) => {
+    await Meal.find({_id: req.params.id})
+    .then((data)=>{
+        res.status(200).json({
+            data: data
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+})
+
+// GET all
+router.get('/', async(req, res) => {
+    await Meal.find({})
+    .then((data)=>{
+        res.status(200).json({
+            data: data
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+})
+
+// GET by status AND meal AND date
+router.get('/', async(req, res) => {
+    
+})
+
+
+/* ==============================================
+    Need to solve the url issue,
+    below operationscan't be done as we have
+    already used '/' url for getting all data
+============================================== */
+
+// GET by Meal name http://localhost:3000/meal?meal=Breakfast
+/* router.get('/', async(req, res) => {
+    await Meal.find({meal: req.query.meal})
+    .then((data)=>{
+        res.status(200).json({
+            data: data
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+}) */
+
+// GET by Date http://localhost:3000/date?date=Breakfast
+/* router.get('/', async(req, res) => {
+    await Meal.find({date: req.query.date})
+    .then((data)=>{
+        res.status(200).json({
+            data: data
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+}) */
+
+
+
 
 module.exports = router;
--- collectionSchemas/mealSchema.js
+++ collectionSchemas/mealSchema.js
@@ -1,5 +1,25 @@
 const mongoose = require("mongoose");
-const mealSchema = mongoose.Schema()
+const date = new Date().toJSON().slice(0,10).split('-').reverse().join('-'); /* new Date().toLocaleDateString(); */
+const time = new Date().toLocaleTimeString();
+const mealSchema = mongoose.Schema({
+    meal: {
+        type: String,
+        enum: ["Breakfast", "Lunch", "Dinner"]
+    },
+    status: {
+        type: Number,
+        enum: [0, 1],
+        default: 0
+    },
+    date: {
+        type: String,
+        default: date
+    },
+    time: {
+        type: String,
+        default: time
+    },
+})
 
 
 module.exports = mealSchema;
