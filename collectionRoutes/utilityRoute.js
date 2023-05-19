244a498ea192b73f49377388a600100d271ae391
ijTuhin <isratishu0209@gmail.com>
2023-05-19 11:45:35 +0600

    Utility collection POST done

--- collectionRoutes/utilityRoute.js
+++ collectionRoutes/utilityRoute.js
@@ -4,4 +4,54 @@ const router = express.Router();
 const utilitySchema = require("../collectionSchemas/utilitySchema")
 const Utility = new mongoose.model("Utility", utilitySchema)
 
+
+// POST new Utility
+router.post('/', async(req, res) => {
+    const newUtility = new Utility(req.body);
+    await newUtility
+    .save()
+    .then(()=>{
+        res.status(200).json({
+            success: "Insertion successful"
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+})
+router.post('/new', async(req, res) => {
+    await Utility.insertMany(req.body)
+    .then(()=>{
+        res.status(200).json({
+            success: "Insertion successful"
+        })
+    })
+    .catch(()=>{
+        res.status(400).json({
+            error: "Oops! Something went wrong!"
+        })
+    })
+})
+
+
 module.exports = router;
+/* 
+============== To store Due to new from previous unpaid utility bill ===============
+const value = [
+    {status: 0,bill: 2000, name: "item1"},
+    {status: 0,bill: 1200, name: "item2"},
+    {status: 1,bill: 2500, name: "item3"},
+    {status: 0,bill: 3000, name: "item4"},
+    {status: 1,bill: 4500, name: "item5"}
+  ]
+  const item = value.map(i => {
+    const items = {
+      due:i.status * i.bill,
+      name: i.name
+    }
+    console.log(i.status * i.bill)
+    return items;
+  })
+*/
--- collectionSchemas/utilitySchema.js
+++ collectionSchemas/utilitySchema.js
@@ -1,5 +1,33 @@
 const mongoose = require("mongoose");
-const utilitySchema = mongoose.Schema({})
+const m = new Date().getMonth();
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const utilitySchema = mongoose.Schema({
+    name: {
+        type: String,
+        enum: ["electricity", "groceries", "gas", "water", "wifi"]
+    },
+    month: {
+        type: String,
+        default: month[m]
+    },
+    status: {
+        type: Number,
+        enum: [0,1],
+        default: 1
+    },
+    bill: {
+        type: Number,
+        default: 0
+    },
+    Due: {
+        type: Number,
+        default: 0
+    },
+    Date: {
+        type: String,
+        default: "---"
+    }
+})
 
 
 module.exports = utilitySchema;
