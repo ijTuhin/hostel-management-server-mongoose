fb72f62c1047b286ec26ae2bcda6955e42804f92
ijTuhin <isratishu0209@gmail.com>
2023-06-06 23:24:06 +0600

    Edited meal order with time condition

--- collectionRoutes/mealRoute.js
+++ collectionRoutes/mealRoute.js
@@ -13,7 +13,8 @@ router.post("/order", checkLogin, async (req, res) => {
                 Set Time Condition
   ================================================ */
   const date = new Date();
-  const time = date.toTimeString().split(":")[0];
+  let time = date.toTimeString().split(":")[0];
+  time = parseInt(time);
   const today = date.toLocaleDateString();
   date.setDate(date.getDate() + 1);
   const tomorrow = date.toLocaleDateString();
@@ -24,7 +25,7 @@ router.post("/order", checkLogin, async (req, res) => {
     meal = "Lunch";
   } else if (time > 13 && time < 18) {
     meal = "Dinner";
-  } else if (time > 19 || time < 2) {
+  } else if (time > 19 || time <= 23) {
     meal = "Breakfast";
     day = tomorrow;
   }
@@ -46,6 +47,61 @@ router.post("/order", checkLogin, async (req, res) => {
     }
   )
     .then(() => res.json(`${newMeal.meal} order for ${newMeal.date}`))
+    .catch(() => res.json("Oops! Something went wrong!"));
+});
+
+// UPDATE received status by Id
+router.put("/:id", checkLogin, async (req, res) => {
+  await Meal.updateOne(
+    { _id: req.params.id },
+    {
+      $set: { status: 1 },
+    }
+  )
+    .then(() => res.json("Meal received"))
+    .catch(() => res.json("Oops! Something went wrong!"));
+});
+
+// DELETE meal request by ID
+router.delete("/:id", checkLogin, async (req, res) => {
+  const meal = await Meal.findOne({ _id: req.params.id });
+  if (!meal.status) {
+    const user = await User.findOne({ _id: req.userId });
+    await Meal.deleteOne({ _id: req.params.id, status: 0 })
+      .then(async () => {
+        await User.updateOne(
+          { _id: req.userId },
+          { $set: { coupon: user.coupon + 1 } }
+        ).then(() => res.json("Deleted"));
+      })
+      .catch(() => res.json("Oops! Something went wrong!"));
+  } else {
+    res.json("Meal Served!");
+  }
+});
+
+// GET by id
+router.get("/:id", checkLogin, async (req, res) => {
+  await Meal.find({ _id: req.params.id })
+    .populate("user", "matric dept room name")
+    .then((data) => res.json(data))
+    .catch(() => res.json("Oops! Something went wrong!"));
+});
+
+// GET all
+router.get("/", async (req, res) => {
+  let query = {};
+  if (req.query.date && req.query.meal) {
+    query = {
+      meal: req.query.meal,
+      date: req.query.date,
+    }; //http://localhost:3001/meal?meal=${meal}&date=${date}
+  } else if (req.query.date) {
+    query = { date: req.query.date }; //http://localhost:3001/meal?date=${date}
+  }
+  await Meal.find(query)
+    .populate("user", "matric dept room name")
+    .then((data) => res.json(data))
     .catch(() => res.json("Oops! Something went wrong!"));
 });
 
