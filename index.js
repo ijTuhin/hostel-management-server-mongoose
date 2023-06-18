76cd27f07a049d76ef74ac6a08ceebe1a00707d4
ijTuhin <isratishu0209@gmail.com>
2023-06-18 13:08:42 +0600

    mongoDB atlas connected

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const userRoute = require("./collectionRoutes/userRoute.js")
-const seatRoute = require("./collectionRoutes/seatRoute.js")
-const paymentRoute = require("./collectionRoutes/paymentRoute.js")
-const mealRoute = require("./collectionRoutes/mealRoute.js")
-const attendanceRoute = require("./collectionRoutes/attendanceRoute.js")
-const utilityRoute = require("./collectionRoutes/utilityRoute.js")
-const staffRoute = require("./collectionRoutes/staffRoute.js")
-const salaryRoute = require("./collectionRoutes/salaryRoute.js")
-const groceryRoute = require("./collectionRoutes/groceryRoute.js")
-const balanceSheetRoute = require("./collectionRoutes/balanceSheetRoute.js")
-const packageRoute = require("./collectionRoutes/packageRoute.js")
-const adminRoute = require("./collectionRoutes/adminRoute.js")
-const noticeRoute = require("./collectionRoutes/noticeRoute.js")
+const userRoute = require("./collectionRoutes/userRoute.js");
+const seatRoute = require("./collectionRoutes/seatRoute.js");
+const paymentRoute = require("./collectionRoutes/paymentRoute.js");
+const mealRoute = require("./collectionRoutes/mealRoute.js");
+const attendanceRoute = require("./collectionRoutes/attendanceRoute.js");
+const utilityRoute = require("./collectionRoutes/utilityRoute.js");
+const staffRoute = require("./collectionRoutes/staffRoute.js");
+const salaryRoute = require("./collectionRoutes/salaryRoute.js");
+const groceryRoute = require("./collectionRoutes/groceryRoute.js");
+const balanceSheetRoute = require("./collectionRoutes/balanceSheetRoute.js");
+const packageRoute = require("./collectionRoutes/packageRoute.js");
+const adminRoute = require("./collectionRoutes/adminRoute.js");
+const noticeRoute = require("./collectionRoutes/noticeRoute.js");
 const app = express();
-// const port = process.env.PORT || 5000;
 
 app.use(cors()); // using middleware
 app.use(express.json()); // to Body parse
 
-require("dotenv").config(); // For storing DBname& password security
+require("dotenv").config(); 
 
 /* *******************
 DB connection with Mongoose
 ******************** */
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12dph.mongodb.net/?retryWrites=true&w=majority`; */
-mongoose
-.connect('mongodb://localhost:27017/hostelManagement', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log('Successful Connection'))
-.catch(err => console.log(err))
+const database = (module.exports = () => {
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dbhost.ueaoans.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("Successful Connection"))
+    .catch((err) => console.log(err));
+});
+
+database()
 
 /* *******************
         Routes
 ******************** */
-app.use('/user', userRoute)
-app.use('/seat', seatRoute)
-app.use('/payment', paymentRoute)
-app.use('/meal', mealRoute)
-app.use('/attendance', attendanceRoute)
-app.use('/utility', utilityRoute)
-app.use('/staff', staffRoute)
-app.use('/salary', salaryRoute)
-app.use('/grocery', groceryRoute)
-app.use('/balanceSheet', balanceSheetRoute)
-app.use('/package', packageRoute)
-app.use('/admin', adminRoute)
-app.use('/notice', noticeRoute)
+app.use("/user", userRoute);
+app.use("/seat", seatRoute);
+app.use("/payment", paymentRoute);
+app.use("/meal", mealRoute);
+app.use("/attendance", attendanceRoute);
+app.use("/utility", utilityRoute);
+app.use("/staff", staffRoute);
+app.use("/salary", salaryRoute);
+app.use("/grocery", groceryRoute);
+app.use("/balanceSheet", balanceSheetRoute);
+app.use("/package", packageRoute);
+app.use("/admin", adminRoute);
+app.use("/notice", noticeRoute);
 
 app.listen(3001, () => {
   console.log("Mongoose Server running");
