c097002491af54379427f10a0430034163bf58e1
ijTuhin <isratishu0209@gmail.com>
2023-06-21 23:51:34 +0600

    modified user meal & rent status

--- collectionRoutes/userRoute.js
+++ collectionRoutes/userRoute.js
@@ -6,6 +6,10 @@ const checkAdminLogin = require("../Authentications/checkAdminLogin.js");
 const checkLogin = require("../Authentications/checkLogin.js");
 const userSchema = require("../collectionSchemas/userSchema.js");
 const User = new mongoose.model("User", userSchema);
+const paymentSchema = require("../collectionSchemas/paymentSchema.js");
+const Payment = new mongoose.model("Payment", paymentSchema);
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const month = months[new Date().getMonth()] + "-" + new Date().getFullYear();
 
 // User SIGN-UP & LOG IN
 router.post("/signup", checkAdminLogin, async (req, res) => {
@@ -51,12 +55,39 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
 // GET data
 router.get("/", checkAdminLogin, async (req, res) => {
   await User.find({})
     .sort({ _id: -1 })
     .select("matric name enroll sem dept room email account")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(400).json({
+        error: "Oops! Something went wrong!",
+      });
+    });
+});
+router.get("/meal-status", checkAdminLogin, async (req, res) => {
+  await User.find({})
+    .sort({ meal: -1 })
+    .select("matric name sem dept room meal")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(400).json({
+        error: "Oops! Something went wrong!",
+      });
+    });
+});
+router.get("/rent-status", checkAdminLogin, async (req, res) => {
+  await User.find({})
+    .sort({ rent: -1 })
+    .select("matric name sem dept room rent")
     .then((data) => {
       res.status(200).json(data);
     })
@@ -135,7 +166,6 @@ router.get("/attendance", checkAdminLogin, async (req, res) => {
     });
 });
 
-
 // UPDATE user data
 router.put("/update/:id", checkAdminLogin, async (req, res) => {
   await User.updateOne(
@@ -155,6 +185,25 @@ router.put("/update/:id", checkAdminLogin, async (req, res) => {
   )
     .then(() => res.json("Updated"))
     .catch(() => res.json("Could not Update"));
+});
+// UPDATE user rent status
+router.put("/update-rent", checkLogin, async (req, res) => {
+  const current = await Payment.findOne({
+    month: month,
+    item: "rent",
+    user: req.userId,
+  });
+  if (!current)
+    await User.updateOne(
+      { _id: req.userId },
+      {
+        $set: {
+          rent: 0,
+        },
+      }
+    )
+      .then(() => res.json("Updated Rent Status"))
+      .catch(() => res.json("Could not Update"));
 });
 // UPDATE account validity
 router.put("/account/:id", checkAdminLogin, async (req, res) => {
@@ -182,7 +231,6 @@ router.put("/account/:id", checkAdminLogin, async (req, res) => {
       .then(() => res.json("Account validated"))
       .catch(() => res.json("Could not Update"));
 });
-
 
 // DELETE user record
 router.delete("/:id", checkAdminLogin, async (req, res) => {
