6ca9f986d9c071e7975c139a5fd0ced1658f3f21
ijTuhin <isratishu0209@gmail.com>
2023-08-08 17:35:28 +0600

    SSLcommerce payment integration testing done User App

--- collectionRoutes/paymentRoute.js
+++ collectionRoutes/paymentRoute.js
@@ -88,7 +88,7 @@ router.post("/meal-package", checkLogin, async (req, res) => {
           user: authID,
           trxID: req.params.trxId,
           package: package,
-          phone: phone
+          phone: phone,
         }).save();
         await BalanceSheet.updateOne(
           { status: 1 },
@@ -106,10 +106,9 @@ router.post("/meal-package", checkLogin, async (req, res) => {
             },
           }
         ).then(() => {
-          console.log('res.redirect(`exp://192.168.0.106:19000`)')
-          res.redirect(`exp://192.168.0.106:19000`)
+          console.log("res.redirect(`exp://192.168.0.106:19000`)");
+          res.redirect(`exp://192.168.0.106:19000`);
         });
-        // res.json(`Payment Successfull with ${newPayment.package} days package`);
         console.log(
           `Payment Successfull with ${newPayment.package} days package`
         );
@@ -119,45 +118,85 @@ router.post("/meal-package", checkLogin, async (req, res) => {
 });
 // POST Seat Rent Payment
 router.post("/seat-rent", checkLogin, async (req, res) => {
+  const authID = req.userId;
+  const phone = req.body.phone;
   const payment = await Payment.findOne({
     user: req.userId,
     item: "rent",
     month: month,
   });
+  const store_id = process.env.SSL_STORE_ID;
+  const store_passwd = process.env.SSL_STORE_PASS;
+  const trxID = "Txr" + Math.random().toString(36).substring(4, 11) + "kdz";
+  const is_live = false;
+  const data = {
+    total_amount: 2500,
+    currency: "BDT",
+    tran_id: trxID,
+    success_url: `http://192.168.0.106:3001/payment/success/${trxID}`,
+    fail_url: "http://192.168.0.106:3001/fail",
+    cancel_url: "http://192.168.0.106:3001/cancel",
+    ipn_url: "http://192.168.0.106:3001/ipn",
+    shipping_method: "Payment",
+    product_name: req.userId,
+    product_category: "Bill",
+    product_profile: "Utility",
+    cus_name: req.userId,
+    cus_email: "customer@example.com",
+    cus_add1: "Ctg, Bangladesh",
+    cus_phone: "0139999999",
+    ship_name: req.userId,
+    ship_add1: "Ctg, Bangladesh",
+    ship_city: "Chattogram",
+    ship_postcode: 4200,
+    ship_country: "Bangladesh",
+  };
+  console.log(data);
   if (!payment) {
-    var newPayment = await new Payment({
-      ...req.body,
-      bill: 2500,
-      item: "rent",
-      user: req.userId,
-    }).save();
-    await User.updateOne(
-      { _id: req.userId },
-      {
-        $push: {
-          payments: newPayment._id,
-        },
-        $set: {
-          rent: 1,
-        },
-      }
-    );
-    await BalanceSheet.updateOne(
-      { status: 1 },
-      {
-        $push: { seatRent: newPayment._id },
-      }
-    )
-      .then(() => {
-        res.status(201).json({
-          message: "Seat rent paid",
+    const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+    sslcz.init(data).then((apiResponse) => {
+      let GatewayPageURL = apiResponse.GatewayPageURL;
+      res.send({ url: GatewayPageURL });
+      console.log("Redirecting to: ", GatewayPageURL);
+    });
+
+    router.post("/success/:trxId", async (req, res) => {
+      console.log("Rent TrxID: ", req.params.trxId, authID);
+      const newPayment = await new Payment({
+        ...req.body,
+        bill: 2500,
+        item: "rent",
+        user: authID,
+        trxID: req.params.trxId,
+        phone: phone,
+      }).save();
+      await User.updateOne(
+        { _id: authID },
+        {
+          $push: {
+            payments: newPayment._id,
+          },
+          $set: {
+            rent: 1,
+          },
+        }
+      );
+      await BalanceSheet.updateOne(
+        { status: 1 },
+        {
+          $push: { seatRent: newPayment._id },
+        }
+      )
+        .then(() => {
+          console.log("res.redirect(`exp://192.168.0.106:19000`)");
+          res.redirect(`exp://192.168.0.106:19000`);
+        })
+        .catch(() => {
+          res.status(400).json({
+            error: "Oops! Something went wrong!",
+          });
         });
-      })
-      .catch(() => {
-        res.status(400).json({
-          error: "Oops! Something went wrong!",
-        });
-      });
+    });
   }
 });
 
